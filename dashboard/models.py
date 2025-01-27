5ec7360c321b81e77be2589e25d91bec04d9652b
beel <nabeelmuhammad1209@gmail.com>
2025-01-27 23:06:46 +0700

    fix(dashboard):adjust model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,29 +1,19 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+DOCUMENT_TYPE_CHOICES = [
+    ('Cooperation Letter', 'Cooperation Letter'),
+    ('Employment Agreement', 'Employment Agreement'),
+    ('Request/Sales Letter', 'Request/Sales Letter'),
+]
 
 class DocumentRequest(models.Model):
-    DOCUMENT_TYPE_CHOICES = [
-        ('Cooperation Letter', 'Cooperation Letter'),
-        ('Employment Agreement', 'Employment Agreement'),
-        ('Request/Sales Letter', 'Request/Sales Letter'),
-    ]
-
-    DOCUMENT_STATUS_CHOICES = [
-        ('Sent for Signature', 'Sent for Signature'),
-        ('Completed', 'Completed'),
-        ('Rejected', 'Rejected'),
-    ]
+    document_title = models.CharField(max_length=100)
     document_type = models.CharField(max_length=50, choices=DOCUMENT_TYPE_CHOICES)
-    document_status = models.CharField(max_length=20, choices=DOCUMENT_STATUS_CHOICES)
     created_date = models.DateTimeField(auto_now_add=True)
     comments_notes = models.TextField()
     expired_date = models.DateTimeField()
-    fullname = models.CharField(max_length=100)
     document_number = models.CharField(max_length=20)
-    link_to_details = models.URLField()
-    link_to_document = models.URLField()
+    envelope_id = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.document_type} - {self.fullname}"
+        return f"{self.document_type} - {self.document_title}"
